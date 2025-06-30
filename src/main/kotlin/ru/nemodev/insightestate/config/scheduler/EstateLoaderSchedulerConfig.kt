22b5f0aa75c84ec2beed18f7b998f209f659f872
Артур Миров <amirov@mmk.local>
2025-06-30 08:01:57 +0300

    tariffs

--- src/main/kotlin/ru/nemodev/insightestate/config/scheduler/EstateLoaderSchedulerConfig.kt
+++ src/main/kotlin/ru/nemodev/insightestate/config/scheduler/EstateLoaderSchedulerConfig.kt
@@ -12,12 +12,12 @@ class EstateLoaderSchedulerConfig(
     private val estateLoader: EstateLoader,
     private val estateImageLoader: EstateImageLoader
 ) {
-    @Scheduled(cron = "\${google.spreadsheets.estate-load-cron}")
+    //@Scheduled(cron = "\${google.spreadsheets.estate-load-cron}")
     fun loadEstateFromGoogleSpreadsheets() {
         estateLoader.loadFromGoogleSpreadsheets()
     }
 
-    @Scheduled(cron = "\${google.drive.estate-image-load-cron}")
+    //@Scheduled(cron = "\${google.drive.estate-image-load-cron}")
     fun loadEstateImageFromGoogleDrive() {
         estateImageLoader.loadFromGoogleDrive()
     }
